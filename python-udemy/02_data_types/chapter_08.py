ec1d0048ac0b9b5e3088c165923bb4e9da473e89
raheelkumar <raheelrsingh@gmail.com>
2026-09-24 18:46:47 +0530

    Basics of list in python

---
+++ python-udemy/02_data_types/chapter_08.py
@@ -0,0 +1,29 @@
+ingredients = ["Water", "Milk", "tea leaves"]
+
+ingredients.append("Sugar")
+print(f"Tea Ingredients are: {ingredients}")
+ingredients.remove("Sugar")
+print(f"Tea Ingredients are: {ingredients}")
+
+spice_options = ["ginger", "cardamom"]
+chai_ingredients = ["water", "milk"]
+print(f"Chai Ingredients: {chai_ingredients}")
+chai_ingredients.extend(spice_options)
+print(f"Masala Chai Ingredients: {chai_ingredients}")
+
+chai_ingredients.insert(2, "tea leaves")
+print(f"Chai Ingredients: {chai_ingredients}")
+
+last_added = chai_ingredients.pop()
+
+print(f"Last item: {last_added}")
+print(f"Chai Ingredients: {chai_ingredients}")
+chai_ingredients.reverse()
+print(f"Reverse Chai Ingredients: {chai_ingredients}")
+
+chai_ingredients.sort()
+print(f"Sorted Chai Ingredients: {chai_ingredients}")
+
+sugar_level = [1,2,3,4,5]
+print(f"Highest sugar level: {max(sugar_level)}")
+print(f"Lowest sugar level: {min(sugar_level)}")
