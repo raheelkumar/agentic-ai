edfeb9f54b69add33614aaf8f50d8755e7b133d5
raheelkumar <raheelrsingh@gmail.com>
2026-09-24 18:57:13 +0530

    Operator overloading and bytearray in python

--- python-udemy/02_data_types/chapter_08.py
+++ python-udemy/02_data_types/chapter_08.py
@@ -27,3 +27,22 @@ print(f"Sorted Chai Ingredients: {chai_ingredients}")
 sugar_level = [1,2,3,4,5]
 print(f"Highest sugar level: {max(sugar_level)}")
 print(f"Lowest sugar level: {min(sugar_level)}")
+
+# Operator overloading
+base_liquid = ["water", "milk"]
+extra_flavor = ["ginger"]
+
+masala_liquid = base_liquid + extra_flavor
+
+print(f"Masala Liquid: {masala_liquid}")
+
+strong_brew = ["black tea"] * 3
+print(f"Strong brew: {strong_brew}")
+
+strong_brew = ["black tea", "Water"] * 3
+print(f"Strong brew: {strong_brew}")
+
+raw_spice_data = bytearray(b"CINNAMON")
+print(f"Raw Spices: {raw_spice_data}")
+raw_spice_data = raw_spice_data.replace(b"CINNA", b"CARD")
+print(f"Raw Spices: {raw_spice_data}")
