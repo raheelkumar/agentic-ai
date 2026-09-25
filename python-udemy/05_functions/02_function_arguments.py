a7a93465189b47bcde383c381b2d3f5e7aa3076e
raheelkumar <raheelrsingh@gmail.com>
2026-09-25 17:36:22 +0530

    args and kwargs

--- python-udemy/05_functions/02_function_arguments.py
+++ python-udemy/05_functions/02_function_arguments.py
@@ -5,5 +5,8 @@ def edit_list(index, name: str):
 
 if __name__ == '__main__':
     print(chai_list)
-    edit_list(1,'Elaichi')
-    print(f"After edit: \n {chai_list}")
+    edit_list(1,'Elaichi') #positional argument
+    print(f"After arguments edit: \n {chai_list}")
+
+    edit_list(index=1,name='Elaichi') #keyword arguments
+    print(f"After keyword arguments edit: \n {chai_list}")
